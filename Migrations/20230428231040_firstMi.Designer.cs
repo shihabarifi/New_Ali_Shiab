74e50076f97beb612af4a4a090836a7154af6aba
Mr.Error_Ali <52839092+Al-Akwaa@users.noreply.github.com>
2023-04-29 22:17:54 +0300

    try a new migration

--- Migrations/20230428231040_firstMi.Designer.cs
+++ Migrations/20230428231040_firstMi.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using POS.Data;
 
@@ -11,9 +12,10 @@ using POS.Data;
 namespace POS.Migrations
 {
     [DbContext(typeof(posDbContext))]
-    partial class posDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230428231040_firstMi")]
+    partial class firstMi
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
