693cafbf7f8aab2c8bc3a1e9f0a77ef7eb32f191
Shihab_Tsunami∆ <71707100+shihabarifi@users.noreply.github.com>
2023-05-02 23:29:20 +0300

    الكل

---
+++ Constants/Permissions.cs
@@ -0,0 +1,54 @@
+﻿using static POS.Models.Helper;
+
+namespace POS.Constants
+{
+    public class Permissions
+    {
+        public static List<string> GeneratePermissionsFromModule(string module)
+        {
+            return new List<string>
+            {
+                $"Permissions.{module}.View",
+                $"Permissions.{module}.Create",
+                $"Permissions.{module}.Edit",
+                $"Permissions.{module}.Delete"
+            };
+        }
+
+        public static List<string> PermissionsList()
+        {
+            var allPermissions = new List<string>();
+
+            foreach (var module in Enum.GetValues(typeof(PermissionModuleName)))
+                allPermissions.AddRange(GeneratePermissionsFromModule(module.ToString()));
+            return allPermissions;
+        }
+        public static class Home {
+            public const string View = "Permissions.Home.View";
+            public const string Create = "Permissions.Home.Create";
+            public const string Edit = "Permissions.Home.Edit";
+            public const string Delete = "Permissions.Home.Delete";
+        }
+        public static class Accounts
+        {
+            public const string View = "Permissions.Accounts.View";
+            public const string Create = "Permissions.Accounts.Create";
+            public const string Edit = "Permissions.Accounts.Edit";
+            public const string Delete = "Permissions.Accounts.Delete";
+        }
+        public static class Roles
+        {
+            public const string View = "Permissions.Roles.View";
+            public const string Create = "Permissions.Roles.Create";
+            public const string Edit = "Permissions.Roles.Edit";
+            public const string Delete = "Permissions.Roles.Delete";
+        }
+        public static class Registers
+        {
+            public const string View = "Permissions.Registers.View";
+            public const string Create = "Permissions.Registers.Create";
+            public const string Edit = "Permissions.Registers.Edit";
+            public const string Delete = "Permissions.Registers.Delete";
+        }
+    }
+}
