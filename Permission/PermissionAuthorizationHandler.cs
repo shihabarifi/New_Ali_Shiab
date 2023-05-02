45db69cdbdd5c308c92642d1e9eb4ecfdf2b0605
Shihab_Tsunami∆ <71707100+shihabarifi@users.noreply.github.com>
2023-05-02 23:17:21 +0300

    اضافة الملفات الناقصة

---
+++ Permission/PermissionAuthorizationHandler.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using POS.Models;
+
+namespace POS.Permission
+{
+    public class PermissionAuthorizationHandler: AuthorizationHandler<PermissionRequirement>
+    {
+        public PermissionAuthorizationHandler()
+        {
+
+        }
+
+        protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context,
+                                                        PermissionRequirement requirement)
+        {
+           if (context.User == null)
+            {
+                return ;
+                var Permission = context.User.Claims
+                    .Where(x => x.Type == Helper.Permission && x.Value == requirement
+                    .Permission && x.Issuer == "LOCAL AUTHORITY");
+                if (Permission.Any())
+                {
+                    context.Succeed(requirement);
+                    return;
+                }
+            }
+
+
+        }
+    }
+}
---
+++ Permission/PermissionPolicyProvider.cs
@@ -0,0 +1,38 @@
+﻿
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Options;
+using POS.Models;
+
+namespace POS.Permission
+{
+    public class PermissionPolicyProvider : IAuthorizationPolicyProvider
+    {
+        public DefaultAuthorizationPolicyProvider FallbackPolicyProvider { get; }
+       
+
+        public PermissionPolicyProvider(IOptions<AuthorizationOptions> options)
+        {
+            FallbackPolicyProvider = new DefaultAuthorizationPolicyProvider(options);   
+        }
+        public Task<AuthorizationPolicy> GetDefaultPolicyAsync()
+        {
+          return  FallbackPolicyProvider.GetFallbackPolicyAsync();
+        }
+
+        public Task<AuthorizationPolicy?> GetFallbackPolicyAsync()
+        {
+            return FallbackPolicyProvider.GetFallbackPolicyAsync();
+        }
+
+        public Task<AuthorizationPolicy?> GetPolicyAsync(string policyName)
+        {
+            if (policyName.StartsWith(Helper.Permission, System.StringComparison.OrdinalIgnoreCase))
+            {
+                var policy = new AuthorizationPolicyBuilder();
+                policy.AddRequirements(new PermissionRequirement(policyName));
+                return Task.FromResult(policy.Build()); 
+            }
+            return FallbackPolicyProvider.GetPolicyAsync(policyName);
+        }
+    }
+}
