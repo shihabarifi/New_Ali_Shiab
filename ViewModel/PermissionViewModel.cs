360011504806e72f6e2a667c645638067eba26b0
Shihab_Tsunami∆ <71707100+shihabarifi@users.noreply.github.com>
2023-05-02 23:32:41 +0300

    خلاص الكل

---
+++ ViewModel/PermissionViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace POS.ViewModel
+{
+    public class PermissionViewModel
+    {
+        public string RoleId { get; set; } = string.Empty;
+
+        public string RoleName { get; set; } = string.Empty;
+
+        public List<RoleClaimsViewModel> RoleClaims { get; set; } = new List<RoleClaimsViewModel>();
+
+    }
+
+    public class RoleClaimsViewModel
+    {
+        public string Value { get; set; } = string.Empty;
+
+        public bool Selected { get; set; }
+    }
+}
