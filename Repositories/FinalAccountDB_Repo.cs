e782b0fabae4351bc8a0f774dd9c59ea3287d40a
Mr.Error_Ali <52839092+Al-Akwaa@users.noreply.github.com>
2023-05-08 00:09:00 +0300

    Add finalAccountsDB_Repo in Repositories

---
+++ Repositories/FinalAccountDB_Repo.cs
@@ -0,0 +1,48 @@
+﻿using POS.Data;
+using POS.Interfaces;
+using POS.Models.DB;
+
+namespace POS.Repositories
+{
+    public class FinalAccountDB_Repo : pay_recie_finRepository<FinalAccountType>
+    {
+        posDbContext Db;
+
+        public FinalAccountDB_Repo(posDbContext _Db)
+        {
+            Db = _Db;
+        }
+
+        public void Add(FinalAccountType entity)
+        {
+           Db.FinalAccountTypes.Add(entity);
+        }
+
+        public void Delete(int id)
+        {
+            var SingleObject=Find(id);
+            Db.FinalAccountTypes.Remove(SingleObject);
+        }
+
+        public FinalAccountType Find(int id)
+        {
+           var singleAccountType=Db.FinalAccountTypes.Find(id);
+            return singleAccountType;
+        }
+
+        public IList<FinalAccountType> List()
+        {
+            return Db.FinalAccountTypes.ToList();
+        }
+
+        public IList<FinalAccountType> List(string CategoryName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, FinalAccountType entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
