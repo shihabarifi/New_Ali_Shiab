9f8db63303b3bef02c2589685f2ffd220d408362
shihabarifi <shihabriotkd@gmail.com>
2023-05-13 14:50:51 -0700

    تعديل تصدير التقارير

--- wwwroot/pages1/sys7.js
+++ wwwroot/pages1/sys7.js
@@ -35,28 +35,74 @@ $(function () {
           next: 'التالي',
           previous: 'السابق'
         }
-      },
+        },
+      
       // Buttons with Dropdown
-      buttons: [
-         {
-           extend: 'print',
-           text: '<i class="bx bx-printer me-2"></i>إصدار تقرير',
-           className: 'btn btn-label-secondary mx-3',
-           exportOptions: { columns: [3, 2, 1, 0] }
-         },
-        {
-          text: '<i class="icon-plus"></i>إضافة عملية',
-          className: 'add-new btn btn-primary mb-3 mb-md-0',
-          attr: {
-            'data-bs-toggle': 'offcanvas',
-            'data-bs-target': '#offcanvas'
-          },
-          init: function (api, node, config) {
-            $(node).removeClass('btn-secondary');
-          }
-        }
-      ],
+        buttons: [{
+            extend: 'collection',
+            text: '<i class="bx bx-printer me-10"></i>Export ',
+            className: 'btn btn-label-secondary mx-10',
+           
+            buttons: [{
+                extend: 'copy',
+                text: '<i class="bx bx-printer me-1"></i>copy ',
+                className: 'btn btn-label-secondary mx-1',
+                exportOptions:
+                {
+                    columns: [1] /// COLUMN INDEX HERE TO EXPORT 
+                }
+            },
+
+            {
+                extend: 'csv',
+                text: '<i class="bx bx-printer me-1"></i>csv ',
+                className: 'btn btn-label-success  mx-1',
+                exportOptions: {
+                    columns: [1] /// COLUMN INDEX HERE TO EXPORT 
+                }
+            },
+            {
+                extend: 'excel',
+                text: '<i class="bx bx-printer me-1"></i>excel ',
+                className: 'btn btn-label-secondary mx-1',
+                exportOptions: {
+                    columns: [1] /// COLUMN INDEX HERE TO EXPORT 
+                }
+            },
+            {
+                extend: 'pdf',
+                text: '<i class="bx bx-printer me-1"></i>طباعة ',
+                className: 'btn btn-label-danger  mx-1',
+                exportOptions: {
+                    columns: [1] /// COLUMN INDEX HERE TO EXPORT 
+                }
+            },
+            {
+                extend: 'print',
+                text: '<i class="bx bx-printer me-1"></i>طباعة ',
+                className: 'btn btn-label-danger mx-1',
+                 customize: function (win) {
+                    $(win.document.body)
+                        .css('font-size', '10pt')
+                        .prepend(
+                            '<div><img src="/Images/Users/61505fd4-c198-4093-9eae-636eec267c33.JPG" class="rounded-circle"></div>'
+                        );
+
+                    $(win.document.body).find('table')
+                        .addClass('compact')
+                        .css('font-size', 'inherit');
+                },
+                exportOptions: { columns: [3, 2, 1, 0] }
+           
+            }
+            ]
+        },
+           
+                   
+            
+        ],
     });
+
   }
 
   // Delete Record
