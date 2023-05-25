beaec466a52ccaa411c835103cc378531124da36
Shihab_Tsunami∆ <71707100+shihabarifi@users.noreply.github.com>
2023-05-25 13:15:57 -0700

    Models\DBتضمين مودلز التقارير

---
+++ Models/DB/Report/SpGetReport.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using POS.Models.DB;
+
+namespace POS.Models.DB.Report
+{
+    public class SpGetReport
+    {
+         
+        public string Main_ExpensVoucher_Number { get; set; } 
+        public double MainExpensVoucherAmount_RLY { get; set; } 
+        
+        public string CurrenName { get; set; } 
+        public string CurreType { get; set; } 
+        public string FundName { get; set; } 
+
+        public DateTime MainExpensVoucherDate { get; set; }
+
+       
+
+    }
+}
---
+++ Models/DB/Report/SpGetReport3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace POS.Models.DB.Report
+{
+    public class SpGetReport3
+    {
+        public string Check_ExpensVoucherNumber { get; set; }
+        public string CheckNumber { get; set; }
+        public string BankName { get; set; }
+        public string CurrenName { get; set; }
+
+        public double CreditAmount_RLY { get; set; }
+        public string CurreType { get; set; }
+       
+        public string ChequesType { get; set; }
+
+        public DateTime CheckDatetime { get; set; }
+    }
+}
